32ce98216d7611dc70eb3d967c5a57fd3b2dd731
ChayLim <156899992+ChayLimm@users.noreply.github.com>
2025-03-04 12:42:44 +0700

    first commit

---
+++ lib/service/locations_service.dart
@@ -0,0 +1,14 @@
+
+import 'package:refactor_pref_service/model/ride/locations.dart';
+
+import '../dummy_data/dummy_data.dart';
+
+////
+///   This service handles:
+///   - The list of available rides
+///
+class LocationsService {
+
+  static const List<Location> availableLocations = fakeLocations; 
+ 
+}
